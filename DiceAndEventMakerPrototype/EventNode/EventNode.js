8ff66f471a947b8a44dd930caf954e9c2b9eda61
Caotinification <nguyenjustin930@gmail.com>
2024-02-19 18:44:25 -0500

    Addition of EventNode class, which is a tree.

---
+++ DiceAndEventMakerPrototype/EventNode/EventNode.js
@@ -0,0 +1,44 @@
+class EventNode {
+    constructor(name){
+        this.name = name
+    }
+    outcomes = new Map()
+    
+    addEvent(eventNode){
+        this.outcomes.set(eventNode.name) = eventNode
+    }
+
+    removeEvent(eventNodeName){
+        this.outcomes.delete(eventNodeName)
+    }
+
+    generateLevels(){ // organize nodes by depth where array idx is depth, and array members are nodes at that depth
+        let levels = new Array()
+        this.#preorder(
+            (node, depth) => {
+                levels[depth] = levels[depth] ? levels[depth]: new Array()
+                levels[depth].push(node)
+        })
+        return levels
+    }
+
+    toString(){ // convert tree to a string. indentation depth reflects the tree's actual depth 
+        let s = ""
+        this.#preorder(
+            (node, depth) => {s = s.concat(`${' '.repeat(2*depth)}${node.name}\n`)}
+        )
+        return s
+    }
+
+    #preorder(apply, depth){ // preorder traversal with depth tracking
+        depth = depth ? depth : 0
+        apply(this, depth)
+        if (this.outcomes.size > 0){
+            for (const outcome of this.outcomes.values()) {
+                outcome.#preorder(apply, depth + 1);
+            }
+        }
+    }
+}
+
+module.exports = EventNode; // Export the class
