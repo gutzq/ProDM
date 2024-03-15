88647173b1a73e4eff1c5dfbfdfc8ade002c1d5e
AliQat <102432054+AliQat@users.noreply.github.com>
2024-03-15 16:19:32 -0400

    test for CI

--- funcTests.spec.mjs
+++ funcTests.spec.mjs
@@ -2,7 +2,7 @@ import * as chai from 'chai';
 const expect = chai.expect;
 
 // I would rather eat shoes than write tests.
-describe('Positive Test Case', () => {
+describe('Positive Test Case+', () => {
   it('should always pass', () => {
     expect(true).to.be.true;
   });
