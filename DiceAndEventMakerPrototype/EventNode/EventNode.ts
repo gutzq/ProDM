e534c50a820d704f71543197b1664f1206d72269
Caotinification <nguyenjustin930@gmail.com>
2024-03-15 17:55:52 -0400

    more TS dependencies..

---
+++ DiceAndEventMakerPrototype/EventNode/EventNode.ts
@@ -0,0 +1,47 @@
+class EventNode {
+    name: String;
+    outcomes = new Map()
+
+    constructor(name: String){
+        this.name = name
+    }
+    
+    addEvent(eventNode: EventNode){
+        this.outcomes.set(eventNode.name, eventNode)
+    }
+
+    removeEvent(eventName: String){
+        this.outcomes.delete(eventName)
+    }
+
+    generateLevels(){ // organize nodes by depth where array idx is depth, and array members are nodes at that depth
+        let levels = new Array()
+        this.preorder(
+            (node, depth) => {
+                levels[depth] = levels[depth] ? levels[depth]: new Array()
+                levels[depth].push(node)
+        })
+        return levels
+    }
+
+    toString(): String { // represent tree as string. shows overall structure. indentation depth for each node reflects its actual depth 
+        let s = ""
+        this.preorder(
+            (node, depth) => {s = s.concat(`${' '.repeat(2*depth)}${node.name}\n`)}
+        )
+        return s
+    }
+
+    private preorder(apply: (atNode: EventNode, depth: number) => void){ // preorder traversal with depth tracking
+        const helper = (depth: number) => {
+            depth = depth ? depth : 0
+            apply(this, depth)
+            if (this.outcomes.size > 0){
+                for (const outcome of this.outcomes.values()) {
+                    outcome.preorder(apply, depth + 1);
+                }
+            }
+        }
+        helper(0)
+    }
+}
