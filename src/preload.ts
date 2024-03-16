12b9e13dda0ab28064bc457dca65e4c468ca733d
AliQat <102432054+AliQat@users.noreply.github.com>
2024-03-16 14:14:48 -0400

    Error 2:14

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { BrowserWindow } = require('electron')
+const win = new BrowserWindow({
+  webPreferences: {
+    preload: 'path/to/preload.js'
+  }
+})
