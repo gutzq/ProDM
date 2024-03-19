8523db1c3198a22fff3d324492ebaae23d056683
Caotinification <nguyenjustin930@gmail.com>
2024-03-19 15:29:07 -0400

    Generator added for EventNode, and general refactoring of code!!

--- src/classes/EventNode/EventNode.ts
+++ src/classes/EventNode/EventNode.ts
@@ -1,6 +1,6 @@
 const DELIMITER = '\t';
 
-class CycleViolationInTreeError extends Error {}
+class CycleViolation extends Error {}
 
 export class EventNode {
     readonly name: String;
@@ -11,7 +11,6 @@ export class EventNode {
     }
     
     add(newEvent: EventNode) {
-        this.invariant(newEvent);
         this.outcomes.set(newEvent.name, newEvent);
     }
 
@@ -19,44 +18,33 @@ export class EventNode {
         this.outcomes.delete(newEvent);
     }
 
+    *[Symbol.iterator](): Generator<[EventNode, number], void, void> {
+        yield* this.preorder(0);
+    }
+
     generateLevels() { // organize nodes by depth where array idx is depth, and array members are nodes at that depth
-        let levels = new Array();
-        this.preorder(
-            (node, depth) => {
-                levels[depth] = levels[depth] ? levels[depth]: new Array()
-                levels[depth].push(node)
-        });
+        let levels: Array<Array<EventNode>> = new Array();
+        for (const [outcome, depth] of this) {
+            levels[depth] = levels[depth] ? levels[depth]: new Array();
+            levels[depth].push(outcome);
+        }
         return levels;
     }
 
     toString(): String { // represent tree as string. shows overall structure. indentation depth for each node reflects its actual depth 
         let s = '';
-        this.preorder(
-            (node, depth) => {s = s.concat(`${DELIMITER.repeat(depth)}${node.name}\n`)}
-        );
+        for (const [outcome, depth] of this) {
+            s = s.concat(`${DELIMITER.repeat(depth)}${outcome.name}\n`)
+        }
         return s;
     }
 
-    private invariant(recentNode: EventNode): Boolean { // what must be true after every insertion. in the case of a tree, no cycles
-        let visitedNodes = new Set<EventNode>([recentNode]);
-        let hasCycle = false;
-        this.preorder((node, _) => { // currently not very efficient, must visit every node. TODO: make it quit upon finding a cycle
-            hasCycle = visitedNodes.has(node);
-            visitedNodes.add(node);
-        })
-        if (hasCycle) {
-            throw new CycleViolationInTreeError(`${recentNode.name} has caused a cycle in ${this.name}.`);
-        }
-        return !hasCycle;
-    }
-    //TODO: make this a generator, figure out how to hide recursive parameter
-    private preorder(apply: (atNode: EventNode, depth: number) => void, depth: number = 0){ // preorder traversal with depth tracking
-        apply(this, depth);
+    private *preorder(depth: number = 0): Generator<[EventNode, number], void, void>{ // preorder traversal with depth tracking
+        yield [this, depth];
         if (this.outcomes.size > 0) {
             for (const outcome of this.outcomes.values()) {
-                outcome.preorder(apply, depth + 1);
+                yield* outcome.preorder(depth + 1);
             }
         }
     }
-
 }
