05f4f8b86adc7c7509ce93ba925fe1357380a041
Caotinification <nguyenjustin930@gmail.com>
2024-03-20 14:32:52 -0400

    EventNode unit tests refactored for cleanliness.

--- tests/EventNode.test.ts
+++ tests/EventNode.test.ts
@@ -14,23 +14,23 @@ describe('EventNode upon initialization:', () => {
 })
 
 describe('EventNode behavior testing:', () => {
-    let root = new EventNode(mockName.concat('0'));
+    let root = new EventNode(mockName.concat('Root'));
     let node1 = new EventNode(mockName.concat('1'));
     
     let node2 = new EventNode(mockName.concat('2'));
     let node21 = new EventNode(mockName.concat('21'));
+    const two = 2;
 
     root.add(node1);
     root.add(node2);
 
     node2.add(node21);
-
-    test(`EventNode should have ${2} immediate children after additions.`, () =>{
-        expect(root.outcomes.size).toBe(2);
+    test(`${root.name} should have ${two} immediate children after additions.`, () =>{
+        expect(root.outcomes.size).toBe(two);
     });
 
-    test('EventNode should have proper tree structure.', () => {
-        expect(root.toString()).toBe('Node0\n\tNode1\n\tNode2\n\t\tNode21\n');
+    test(`${root.name} should have proper tree structure.`, () => {
+        expect(root.toString()).toBe(`${root.name}\n\t${node1.name}\n\t${node2.name}\n\t\t${node21.name}\n`);
     });
 
     test('EventNode\'s invariant should throw for invalid node addition.', () => {
