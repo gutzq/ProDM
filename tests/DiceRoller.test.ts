a199d9cde96ed959ff2c2065a37056ed1567e403
Caotinification <nguyenjustin930@gmail.com>
2024-03-24 17:42:57 -0400

    new tests

--- tests/DiceRoller.test.ts
+++ tests/DiceRoller.test.ts
@@ -1,12 +1,24 @@
 import { DiceRoller } from "../src/classes/DiceRoller/DiceRoller"
 
-describe('Dice Roller upon Initialization:', () => {
-    const six = 6;
-    let d6 = new DiceRoller(six);
-    test(`D6 shall have ${six} PMF values.`, () => {
-        expect(d6.pmf).toHaveLength(six);
-    }),
-    test(`D6 shall have ${six} CDF values.`, () => {
-        expect(d6.cdf).toHaveLength(six);
-    })
-})
+describe('DiceRoller upon Initialization:', () => {
+    const n = 6;
+    let dn = new DiceRoller(n);
+    
+    test(`D6 shall have ${n} PMF values.`, () => {
+        expect(dn.pmf).toHaveLength(n);
+    });
+    
+    test(`D6 shall have ${n} CDF values.`, () => {
+        expect(dn.cdf).toHaveLength(n);
+    });
+
+    test(`D6's PMF values shall sum to 1.`, () => {
+        expect(dn.pmf.reduce((acc, x) => acc + x)).toBeCloseTo(1);
+    });
+
+    
+});
+
+describe('DiceRoller Behavior Testing.', () => {
+    
+});
