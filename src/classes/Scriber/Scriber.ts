ee3eac341a0cff71a740e30f677f19b82b3595b2
Caotinification <nguyenjustin930@gmail.com>
2024-03-27 17:27:55 -0400

    NEEDS CLEANING UP! Functional for now.

--- src/classes/Scriber/Scriber.ts
+++ src/classes/Scriber/Scriber.ts
@@ -2,27 +2,37 @@ import * as fs from 'fs';
 import { EventNode } from '../EventNode/EventNode';
 import { Character } from '../Character/Character';
 
-export const characterSaveFileName = '/Characters.json';
-export const eventsSaveFileName = '/Events.json';
+export const characterSaveFileName = 'Characters.json';
+export const eventsSaveFileName = 'Events.json';
 
 export class Scriber {
     readonly saveDirectory: fs.PathOrFileDescriptor;
 
     constructor(saveDirectory: fs.PathOrFileDescriptor) {
         this.saveDirectory = saveDirectory;
-        fs.appendFileSync(this.saveDirectory + characterSaveFileName, '');
-        fs.appendFileSync(this.saveDirectory + eventsSaveFileName, ''); // this is to create the file just in case. TS/JS has no method of just making one
+        if (!fs.existsSync(this.saveDirectory.toString())) {
+            fs.mkdirSync(this.saveDirectory.toString());
+        }
+        this.initSaveFile(characterSaveFileName);
+        this.initSaveFile(eventsSaveFileName);
     }
 
     saveEventCard(event: EventNode, imgPath: fs.PathOrFileDescriptor) {
-        
+        //TODO
     }
 
     saveCharacter(character: Character) {
         const newCharacter = {stats: Object.fromEntries(character.stats)};
-        let file = fs.readFileSync(this.saveDirectory + characterSaveFileName, 'utf-8');
+        let file = fs.readFileSync(this.saveDirectory + '/' + characterSaveFileName, 'utf-8');
         let characters = JSON.parse(file);
         characters[character.name.toString()] = newCharacter;
-        fs.writeFileSync(this.saveDirectory + characterSaveFileName, JSON.stringify(characters));
+        fs.writeFileSync(this.saveDirectory + '/' + characterSaveFileName, JSON.stringify(characters));
+    }
+
+    private initSaveFile(saveFileName: string){
+        const savePath = `${this.saveDirectory}/${saveFileName}`;
+        if (!fs.existsSync(savePath)) {
+            fs.writeFileSync(savePath, '{}');
+        }
     }
 }
