c0ecc2ca9f2bbbc775cd25ac9eaf60dcc3c94b5a
Caotinification <nguyenjustin930@gmail.com>
2024-03-27 00:14:39 -0400

    Scriber class first commit

--- src/classes/Scriber/Scriber.ts
+++ src/classes/Scriber/Scriber.ts
@@ -1,3 +1,27 @@
-export class Scriber {
+import * as fs from 'fs';
+import { EventNode } from '../EventNode/EventNode';
+import { Character } from '../Character/Character';
 
+const characterSaveFileName = '/Characters.json';
+const eventsSaveFileName = '/Events.json';
+
+export class Scriber {
+    readonly saveDirectory: fs.PathOrFileDescriptor;
+
+    constructor(saveDirectory: fs.PathOrFileDescriptor) {
+        this.saveDirectory = saveDirectory;
+    }
+
+    saveEventCard(event: EventNode, imgPath: fs.PathOrFileDescriptor) {
+        
+    }
+
+    saveCharacter(character: Character) {
+        const newCharacter = {stats: Object.fromEntries(character.stats)};
+        let characters = JSON.parse(
+            fs.readFileSync(this.saveDirectory + characterSaveFileName, 'utf-8')
+        );
+        characters[character.name.toString()] = newCharacter;
+        fs.writeFile(this.saveDirectory + characterSaveFileName, JSON.stringify(characters), (err) => {if (err) throw err;});
+    }
 }
